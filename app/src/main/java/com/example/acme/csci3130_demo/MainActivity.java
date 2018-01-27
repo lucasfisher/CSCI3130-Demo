46386efe81930766168246677f8d26d0e8039f00
lucas fisher <lucasafisher@gmail.com>
2018-01-27 15:35:55 -0400

    Adding The set Text method

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // find the button and define its onClick behaviour
+
+
+    public void setText(View v) {
+        EditText text1 = findViewById(R.id.editText1);
+        String theText = text1.getText().toString();
+        System.out.println(theText);
+        TextView textDisplay =  findViewById(R.id.textView);
+        textDisplay.setText(theText);
+
+    }
+
 }
